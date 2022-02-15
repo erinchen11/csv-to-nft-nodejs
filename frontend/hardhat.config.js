d8a565a8fbb4a1e279ebe346b909c8c5c5862a2b
Ajak Chen <louisay11@gmail.com>
2022-02-15 13:47:09 +0800

    Update hardhat.config.js

--- frontend/hardhat.config.js
+++ frontend/hardhat.config.js
@@ -1,8 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
-// require('dotenv').config();
-// const {RINKEBY_URL, PRIVATE_KEY} = process.env;
-const RINKEBY_URL="https://rinkeby.infura.io/v3/3bc53211d94d46a683714244969e8f9b"
-const PRIVATE_KEY="1a9c43180299c4ad6e0ee8ec6e801aa4d0a9bd14f7e1516133e3e10d17522a68"
+require('dotenv').config();
+const {RINKEBY_URL, PRIVATE_KEY} = process.env;
+
 console.log(RINKEBY_URL);
 console.log(PRIVATE_KEY);
 module.exports = {
